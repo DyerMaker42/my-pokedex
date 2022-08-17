3a830ab5df7c5fc730672fe96e698caa0095ed2f
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 14:36:02 -0700

    added styling for single stats, next to add base stats

--- pages/card.js
+++ pages/card.js
@@ -1,15 +1,30 @@
 import next from "next";
+import Image from "next/image";
 
 const PokeCard = (props) => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>Height:{props.height}</h3>
-      <h3>Weight:{props.weight}</h3>
-      <h3>Type: {props.types[0].type.name}</h3>
-      <h3>Ability: {props.abilities[0].ability.name}</h3>
-      <h3>Base Stats:</h3>
-      <p>(hp/atk/def/special-atk/special-def/speed)</p>
+    <div className="flex justify-center">
+      <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+        <Image
+          // className=" w-full h-full md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+          width={150}
+          height={75}
+          src={props.sprites.front_default}
+          alt=""
+        />
+        <div className="p-6 flex flex-col justify-start">
+          <h5 className="text-gray-900 text-xl font-medium mb-2">
+            {props.name}
+          </h5>
+          <h3 className="text-l">Height:{props.height}</h3>
+          <h3 className="text-l">Weight:{props.weight}</h3>
+          <h3 className="text-l">Type: {props.types[0].type.name}</h3>
+          <h3 className="text-l">Ability: {props.abilities[0].ability.name}</h3>
+          <h3 className="text-l">
+            Base Stats: (hp/atk/def/special-atk/special-def/speed)
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
