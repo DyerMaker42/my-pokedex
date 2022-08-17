f79cbac99e48cd66612c5d2b8c50d5263cdc2273
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 15:41:34 -0700

    properly rendering array of urls to be turned into promises

--- pages/getRandomPokemon.js
+++ pages/getRandomPokemon.js
@@ -3,15 +3,26 @@ import axios from "axios";
 
 const useGetRandomPokemon = () => {
   //so that nothing will be done with random until loading is complete
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   //random will be array of pokemon objects to ne later iterated over
   const [random, setRandom] = useState([]);
 
   const fetchData = () => {
+    //array of numbers
+    //create promise
+    //  one promise per number to api endpoint for a pokemon number
+    //put all promises in array
+    // put inside of a promise.all so requests run concurrently (thanks nelson and nader)
+    // set random to be the resultion of that promise all
+    //set loading to false when promise resolves
     try {
-      //array of elements all undefined
-      let nums = Array.from({ length: 10 });
+      //array of random whole numbers with ceiling of 905
+      let nums = Array.from({ length: 10 }, () =>
+        Math.floor(Math.random() * 905)
+      );
       console.log(nums);
+      let urls = nums.map((num) => "https://pokeapi.co/api/v2/pokemon/" + num);
+      console.log(urls);
     } catch {}
   };
   return (
