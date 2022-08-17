1b40a3210b3d759e2da6bb89b9ddd4b762fed9e2
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 14:54:20 -0700

    added all stats

--- pages/card.js
+++ pages/card.js
@@ -7,23 +7,30 @@ const PokeCard = (props) => {
       <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
         <Image
           // className=" w-full h-full md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-          width={150}
-          height={75}
+          width={300}
+          height={250}
           src={props.sprites.front_default}
           alt=""
         />
-        <div className="p-6 flex flex-col justify-start">
-          <h5 className="text-gray-900 text-xl font-medium mb-2">
+        <ul className="p-6 flex flex-col justify-start">
+          <h3 className="text-gray-900 text-xl font-medium mb-2">
             {props.name}
-          </h5>
-          <h3 className="text-l">Height:{props.height}</h3>
-          <h3 className="text-l">Weight:{props.weight}</h3>
-          <h3 className="text-l">Type: {props.types[0].type.name}</h3>
-          <h3 className="text-l">Ability: {props.abilities[0].ability.name}</h3>
-          <h3 className="text-l">
-            Base Stats: (hp/atk/def/special-atk/special-def/speed)
           </h3>
-        </div>
+          <li className="text-l">Height:{props.height}</li>
+          <li className="text-l">Weight:{props.weight}</li>
+          <li className="text-l">Type: {props.types[0].type.name}</li>
+          <li className="text-l">Ability: {props.abilities[0].ability.name}</li>
+          <li className="text-l">
+            Base Stats:
+            {props.stats.map((stat, id) => {
+              return (
+                <h4 key={id}>
+                  {stat.stat.name}:{stat.base_stat}
+                </h4>
+              );
+            })}
+          </li>
+        </ul>
       </div>
     </div>
   );
