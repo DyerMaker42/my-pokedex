fe5a83f675f5fb380e40230e1ab1c5832d7135c9
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 15:29:02 -0700

    made file to contain useGetRandomPokemon which will be the hook used to handle the api calls for ten different pokemon and returning an array of object, as well as the loading state

---
+++ pages/getRandomPokemon.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import axios from "axios";
+
+const useGetRandomPokemon = () => {
+  const [loading, setLoading] = useState(false);
+  const [random, setRandom] = useState([]);
+};
