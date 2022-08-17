71ed970a971e0dbc5248421c135b04d3f67abbe2
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 15:31:24 -0700

    defined functions in getrandpoke with commetns

--- pages/getRandomPokemon.js
+++ pages/getRandomPokemon.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 import axios from "axios";
 
 const useGetRandomPokemon = () => {
+  //so that nothing will be done with random until loading is complete
   const [loading, setLoading] = useState(false);
+  //random will be array of pokemon objects to ne later iterated over
   const [random, setRandom] = useState([]);
+
+  const fetchData = () => {};
+  return <div></div>;
 };
