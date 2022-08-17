e95e2fded9d4b17f7e8525f8f7d7d3b3e8b1d81a
Nicholas Kotsos <nick.kotsos@gmail.com>
2022-08-17 12:42:08 -0700

    installed packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["pages/index.js"],
+  content: [
+    "pages/index.js",
+    "./pages/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
